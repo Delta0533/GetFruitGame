44d55f046d0634d22f012c9e317c3c345a5dd7ac
PrompipatJa <144836590+Delta0533@users.noreply.github.com>
2023-09-28 23:25:54 +0700

    Create mainMenu.h

---
+++ mainMenu.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "game.h"
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+class mainMenu
+{
+public:
+	mainMenu(sf::RenderWindow &window);
+	void Update(sf::Event &event, sf::RenderWindow &window);
+	void Draw(sf::RenderWindow& window);
+	void Loop(sf::RenderWindow& window);
+
+	sf::Texture bgTexture;
+	sf::Sprite bg;
+
+	sf::Font font;
+	sf::Text menuplay;
+	sf::Text menunexit;
+	sf::Text menuscore;
+};
+
