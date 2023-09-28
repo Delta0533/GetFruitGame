43f3f003a9668b53fd26e6aa0390ff4724ad9d97
PrompipatJa <144836590+Delta0533@users.noreply.github.com>
2023-09-28 23:23:55 +0700

    Update Gui.cpp

--- Gui.cpp
+++ Gui.cpp
@@ -6,23 +6,30 @@ void gui::Init()
 
 	hpText.setFont(font);
 	hpText.setFillColor(sf::Color::White);
-	hpText.setPosition(0, 0);
+	hpText.setPosition(10, 0);
 	hpText.setCharacterSize(28);
 
 	scoreText.setFont(font);
 	scoreText.setFillColor(sf::Color::White);
-	scoreText.setPosition(0, 40);
+	scoreText.setPosition(10, 40);
 	scoreText.setCharacterSize(28);
+
+	waveText.setFont(font);
+	waveText.setFillColor(sf::Color::White);
+	waveText.setPosition(1140,0);
+	waveText.setCharacterSize(28);
 }
 
-void gui::Update(int playerHP, int playerScore)
+void gui::Update(int playerHP, int playerScore,int wave)
 {
-	hpText.setString("HP = " + std::to_string(playerHP));
-	scoreText.setString("Score = " + std::to_string(playerScore));
+	hpText.setString("HP : " + std::to_string(playerHP));
+	scoreText.setString("Score : " + std::to_string(playerScore));
+	waveText.setString("Wave : " + std::to_string(wave));
 }
 
 void gui::Draw(sf::RenderWindow& window)
 {
 	window.draw(hpText);
 	window.draw(scoreText);
+	window.draw(waveText);
 }
