99b863d7ce78e1dee0b8f526385f188089265f53
PrompipatJa <144836590+Delta0533@users.noreply.github.com>
2023-09-28 23:24:32 +0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 
+
 Game::Game(sf::RenderWindow &window) {
 
 	//Wave
@@ -51,16 +52,16 @@ Game::Game(sf::RenderWindow &window) {
 void Game::Update(sf::Event& event, sf::RenderWindow& window)
 {
 	// Check if game is over || Comment to use God Mode so the game never over
-	//if (player.HP <= 0) isGameOver = true;  //GM mode
+	if (player.HP <= 0) isGameOver = true;  //GM mode
 
 	while (window.pollEvent(event)) {
 		if (event.type == sf::Event::Closed) {
 			window.close();
 		}
 		if (event.type == sf::Event::KeyReleased) {
-			// Escape
+			// push Esc to open menu
 			if (event.key.code == sf::Keyboard::Escape) {
-				window.close();
+				mainMenu menu(window);
 			}
 		}
 	}
@@ -82,41 +83,47 @@ void Game::Update(sf::Event& event, sf::RenderWindow& window)
 	if (player.score < 50) {
 		enemySpd = 5;
 		player.speed = 8;
+		player.wave = 0;
 	}
 	//wave 1 
 	else if (player.score >= 50 && player.score < 100 && isWaveBuff[0] == false) {
 		player.HP += 15;
+		player.wave = 1;
 		enemySpd = 7;
 		player.speed = 9;
 		isWaveBuff[0] = true;
 	}
 	//wave 2 
-	 else if (player.score >= 100 && player.score < 200 && isWaveBuff[0] == false) {
+	 else if (player.score >= 100 && player.score < 200 && isWaveBuff[1] == false) {
 		player.HP += 20;
+		player.wave = 2;
 		enemySpd = 9;
 		player.speed = 10;
-		isWaveBuff[0] = true;
+		isWaveBuff[1] = true;
 	}
 	//wave 3 
-	else if (player.score >= 200 && player.score < 300 && isWaveBuff[0] == false) {
+	else if (player.score >= 200 && player.score < 300 && isWaveBuff[2] == false) {
 		player.HP += 25;
+		player.wave = 3;
 		enemySpd = 10;
 		player.speed = 11;
-		isWaveBuff[0] = true;
+		isWaveBuff[2] = true;
 	}
 	//wave 4
-	else if (player.score >= 300 && player.score < 400 && isWaveBuff[0] == false) {
+	else if (player.score >= 300 && player.score < 400 && isWaveBuff[3] == false) {
 		player.HP += 35;
+		player.wave = 4;
 		enemySpd = 12;
-		player.speed = 12;
-		isWaveBuff[0] = true;
-	}
-	//wave 4
-	else if (player.score >= 400 && player.score < 500 && isWaveBuff[0] == false) {
-		player.HP += 55;
-		enemySpd = 15;
 		player.speed = 13;
-		isWaveBuff[0] = true;
+		isWaveBuff[3] = true;
+	}
+	//wave 5
+	else if (player.score >= 400 && player.score < 500 && isWaveBuff[4] == false) {
+		player.HP += 55;
+		player.wave = 5;
+		enemySpd = 15;
+		player.speed = 15;
+		isWaveBuff[4] = true;
 	}
 
 	if (buff.status) {
@@ -133,7 +140,7 @@ void Game::Update(sf::Event& event, sf::RenderWindow& window)
 
 	buff.speedBuff(player.speed);
 
-	gui.Update(player.HP, player.score);
+	gui.Update(player.HP, player.score,player.wave);
 }
 
 void Game::Draw(sf::RenderWindow &window)
@@ -159,5 +166,6 @@ void Game::Loop(sf::RenderWindow &window)
 		Draw(window);
 		window.display();
 	}
-	window.close();
+	//window.close();
+	Gameover gameover(window);
 }
