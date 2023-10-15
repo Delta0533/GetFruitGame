c33c1e188467b04963f5f2e649560247fc058703
PrompipatJa <144836590+Delta0533@users.noreply.github.com>
2023-10-16 03:40:05 +0700

    Update Gameover.h

--- Gameover.h
+++ Gameover.h
@@ -1,21 +1,28 @@
 #pragma once
 
 #include "mainMenu.h"
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+
+#include <fstream>
 
 class Gameover
 {
 
 public:
-	Gameover(sf::RenderWindow& window);
-	void Update(sf::Event& event, sf::RenderWindow& window);
+	Gameover(sf::RenderWindow& window, int playerScore);
+	void Update(sf::Event& event, sf::RenderWindow& window, int playerScore);
 	void Draw(sf::RenderWindow& window);
-	void Loop(sf::RenderWindow& window);
+	void Loop(sf::RenderWindow& window, int playerScore);
+	void saveScore(int playerScore);
 
 	sf::Texture bgtexture;
 	sf::Sprite bg;
+
+	sf::Font font;
+	std::string playerName;
+	sf::Text playerNameText;
+
 
 };
 
