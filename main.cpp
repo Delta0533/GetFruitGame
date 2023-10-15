994239364ff0e0e43db6060a7ccacc78d814cd39
PrompipatJa <144836590+Delta0533@users.noreply.github.com>
2023-10-16 03:42:39 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 
 int main()
 {
+
     srand(time(NULL));
 
     // --- Init Window ---
     sf::ContextSettings settings;
     settings.antialiasingLevel = 4;
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Fruits Slayer", sf::Style::Resize);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Fruits Slayer", sf::Style::Fullscreen);
     window.setFramerateLimit(60);
     // --- Init Window ---
 
